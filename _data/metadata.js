eab1bc32a64d35369e12c0b5281459e1ab1135a3
david <david@dgddev1>
2023-08-09 15:33:36 +0100

    minus logo

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
-	title: "sheepdog automation and ai",
+	title: "sheepdog",
 	url: "https://sh.eep.dog/",
 	language: "en",
-	description: "automation and ai devlopement in midwales",
+	description: "automation and ai devlopement studio in midwales",
 }
